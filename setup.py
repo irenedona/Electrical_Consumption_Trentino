27439601175bc512fa777c135ae62ba95a8f2ec3
GitIreHub <109944207+GitIreHub@users.noreply.github.com>
2022-07-25 08:53:56 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Progetto di data science sui consumi elettrici del trentino',
+    author='Irene Dona, Riccardo Toniolo',
+    license='MIT',
+)
